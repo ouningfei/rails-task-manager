912b876c3cf400fcdf9d952a999f237b71bf7d23
ningfei ou <ouningfei@gmail.com>
2020-06-24 13:07:30 +0800

    incomplete rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,10 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
   def show
     @task = Task.find(params[:id])
   end
